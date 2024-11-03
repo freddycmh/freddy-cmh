858f6f30e4cab16a4d54af84fc48c48df18baabe
freddycmh <50223798+freddycmh@users.noreply.github.com>
2024-11-03 11:47:41 -0500

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import logo from "../assets/fcmhLogo.png";
+import {
+  FaGithub,
+  FaInstagram,
+  FaLinkedin,
+  FaSquareXTwitter,
+} from "react-icons/fa6";
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <a href="/" aria-label="Home">
+          <img src={logo} alt="Logo" className="mx-2" width={150} height={99} />
+        </a>
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a
+          href="https://www.linkedin.com/in/freddy-c-mok-ho-601215197/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://github.com/freddycmh?tab=repositories"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Github"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.instagram.com/freddy_cmh/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Instagram"
+        >
+          <FaInstagram />
+        </a>
+        {/* <a
+          href="https://www.linkedin.com/in/freddy-c-mok-ho-601215197/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Twitter"
+        >
+          <FaSquareXTwitter />
+        </a> */}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/constants/index.js
@@ -0,0 +1,67 @@
+import project1 from "../assets/projects/project-1.webp";
+import project2 from "../assets/projects/project-2.webp";
+import project3 from "../assets/projects/project-3.webp";
+import project4 from "../assets/projects/project-4.webp";
+
+export const HERO_CONTENT = `I am a full-stack developer skilled in building efficient, user-focused applications. With experience in React, Node.js, MongoDB, and Express.js, I’ve developed projects that improve user engagement and streamline functionality. My goal is to leverage these skills to create impactful, reliable digital solutions.
+`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "Full Stack Developer",
+    company: "Battery Park City Residential LLC",
+    description: `Developed a real estate marketplace.Implemented user-centric features like image upload, property tools, and advanced search, boosting user engagement by 50%, while optimizing state management with Redux Toolkit to support a 65% increase in demand and deploying the app on Render, which improved sales and customer retention.`,
+    technologies: [ "React.js",
+  "Tailwind CSS",
+  "Node.js",
+  "Express.js",
+  "MongoDB",
+  "Redux Toolkit",
+  "JWT (JSON Web Tokens)",
+  "Firebase",
+  "Google OAuth"],
+  },
+  {
+    year: "2023 - 2024",
+    role: "Full Stack Developer",
+    company: "Diskko",
+    description: `Optimized web conversion tools and improved UX/UI with React.js, achieving a 6% increase in online sales and an 11% decrease in bounce rates. Leveraged Three.js and Valtio to implement a 3D shirt customization feature, boosting online sales by 12% and user engagement by 15%, while employing a mobile-first design approach to enhance mobile user satisfaction by 40%.`,
+    technologies: ["React.js",
+  "Three.js",
+  "Node.js",
+  "MongoDB",
+  "Valtio",
+  "Git"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Class Helper Platform",
+    image: project1,
+    description:
+      "Developed a web platform to help college students connect through class IDs, enhancing academic peer networking and collaboration. Integrated user profiles with social media links and professor ratings, and led bi-weekly meetings to drive platform improvements and usability.",
+    technologies: ["HTML", "CSS","JavaScript", "React", "Node.js"],
+  },
+  {
+    title: "EverUp Micro-Credential in Software Engineering by CUNY and The New York Jobs CEO Council",
+    image: project2,
+    description:
+      "Completed the EverUp Micro-Credential in Software Engineering, gaining hands-on experience with HTML, CSS, JavaScript, React.js, Node.js, and MongoDB. Built a project portfolio that highlighted advanced skills in responsive design, JavaScript fundamentals, and dynamic web development techniques.",
+    technologies: ["HTML",
+    "CSS",
+    "JavaScript",
+    "React.js",
+    "Node.js",
+    "MongoDB"],
+  },
+];
+
+export const CONTACT = {
+  address: "Brooklyn, NY, USA",
+  phoneNo: "+1 646-595-6787 ",
+  email: "freddychawamokho2001@gmail.com",
+};
